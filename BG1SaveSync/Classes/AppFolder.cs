452c7a9a8fbef6c5b82425ac3fa3b2fa992ae566
Thaurin <thaurin@gmail.com>
2018-02-09 15:20:24 +0100

    Add initial AppFolder class

---
+++ BG1SaveSync/Classes/AppFolder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace BG1SaveSync.Classes
+{
+    public class AppFolder
+    {
+        public string AppFolderLocation;
+        public string TempFolderLocation => $"{AppFolderLocation}\\Temp";
+        public string LastError;
+
+        public AppFolder()
+        {
+            AppFolderLocation = $"{Environment.GetFolderPath(Environment.SpecialFolder.Personal)}\\BG1SaveSync";
+        }
+
+        public bool Initialize()
+        {
+            if (!Directory.Exists(AppFolderLocation))
+            {
+                try
+                {
+                    Directory.CreateDirectory(AppFolderLocation);
+                    Directory.CreateDirectory(TempFolderLocation);
+                }
+                catch (Exception ex)
+                {
+                    LastError = ex.Message;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
