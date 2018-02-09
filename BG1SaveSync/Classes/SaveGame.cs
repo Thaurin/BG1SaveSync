20b3db81019b61209d47555c5269d047db1058a3
Thaurin <thaurin@gmail.com>
2018-02-09 19:00:34 +0100

    Move SaveGame class to Classes directory

---
+++ BG1SaveSync/Classes/SaveGame.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BG1SaveSync.Classes
+{
+    public class SaveGame
+    {
+        public string FullName { get; set; }
+        public string Name { get; set; }
+        public DateTime Date;
+        public string DateTimeString => $"{Date.ToShortDateString()} {Date.ToShortTimeString()}";
+
+        public SaveGame(DirectoryInfo dirInfo)
+        {
+            FullName = dirInfo.Name;
+            Name = FullName.Substring(FullName.LastIndexOf('\\') + 11);
+            Date = dirInfo.CreationTime;
+        }
+
+        public SaveGame(FileInfo fileInfo)
+        {
+            FullName = fileInfo.Name;
+            Name = FullName.Substring(0, FullName.Length - ".bg1save".Length);
+            Date = fileInfo.CreationTime;
+        }
+
+        public static List<SaveGame> GetSaveGamesFromSaveGameDirectory(string directory)
+        {
+            if (Directory.Exists(directory))
+            {
+                List<SaveGame> saveGameList = new List<SaveGame>();
+                DirectoryInfo[] saveDirs = new DirectoryInfo(directory).GetDirectories().OrderByDescending(p => p.CreationTime).ToArray();
+                foreach (DirectoryInfo dirInfo in saveDirs)
+                {
+                    saveGameList.Add(new SaveGame(dirInfo));
+                }
+
+                return saveGameList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static List<SaveGame> GetSaveGamesFromSharedDirectory(string directory)
+        {
+            if (Directory.Exists(directory))
+            {
+                List<SaveGame> saveGameList = new List<SaveGame>();
+                FileInfo[] saveFiles = new DirectoryInfo(directory).GetFiles("*.bg1save").OrderByDescending(p => p.CreationTime).ToArray();
+                foreach (FileInfo file in saveFiles)
+                {
+                    saveGameList.Add(new SaveGame(file));
+                }
+
+                return saveGameList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- BG1SaveSync/MainWindow.xaml.cs
+++ BG1SaveSync/MainWindow.xaml.cs
@@ -20,66 +20,6 @@ using BG1SaveSync.Classes;
 
 namespace BG1SaveSync
 {
-    public class SaveGame
-    {
-        public string FullName { get; set; }
-        public string Name { get; set; }
-        public DateTime Date;
-        public string DateTimeString => $"{Date.ToShortDateString()} {Date.ToShortTimeString()}";
-
-        public SaveGame(DirectoryInfo dirInfo)
-        {
-            FullName = dirInfo.Name;
-            Name = FullName.Substring(FullName.LastIndexOf('\\') + 11);
-            Date = dirInfo.CreationTime;
-        }
-
-        public SaveGame(FileInfo fileInfo)
-        {
-            FullName = fileInfo.Name;
-            Name = FullName.Substring(0, FullName.Length - ".bg1save".Length);
-            Date = fileInfo.CreationTime;
-        }
-
-        public static List<SaveGame> GetSaveGamesFromSaveGameDirectory(string directory)
-        {
-            if (Directory.Exists(directory))
-            {
-                List<SaveGame> saveGameList = new List<SaveGame>();
-                DirectoryInfo[] saveDirs = new DirectoryInfo(directory).GetDirectories().OrderByDescending(p => p.CreationTime).ToArray();
-                foreach (DirectoryInfo dirInfo in saveDirs)
-                {
-                    saveGameList.Add(new SaveGame(dirInfo));
-                }
-
-                return saveGameList;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public static List<SaveGame> GetSaveGamesFromSharedDirectory(string directory)
-        {
-            if (Directory.Exists(directory))
-            {
-                List<SaveGame> saveGameList = new List<SaveGame>();
-                FileInfo[] saveFiles = new DirectoryInfo(directory).GetFiles("*.bg1save").OrderByDescending(p => p.CreationTime).ToArray();
-                foreach (FileInfo file in saveFiles)
-                {
-                    saveGameList.Add(new SaveGame(file));
-                }
-
-                return saveGameList;
-            }
-            else
-            {
-                return null;
-            }
-        }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
