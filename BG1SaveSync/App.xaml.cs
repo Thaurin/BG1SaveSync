9fb8c4c5f2348799713ddeacaa6f5e10775e72c0
Thaurin <thaurin@gmail.com>
2018-02-10 01:51:20 +0100

    Make app a single instance application

--- BG1SaveSync/App.xaml.cs
+++ BG1SaveSync/App.xaml.cs
@@ -6,12 +6,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace BG1Sync
+using Microsoft.Shell;
+
+namespace BG1SaveSync
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        private const string Unique = "BG1SaveSync_SingleInstance";
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+
+                application.InitializeComponent();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        #region ISingleInstanceApp Members
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments of second instance
+
+            MainWindow.Activate();
+            return true;
+        }
+        #endregion
     }
 }
