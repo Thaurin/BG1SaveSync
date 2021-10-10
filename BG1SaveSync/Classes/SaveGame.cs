8b8677ecca7055b5f37c7f2e369f3f0f9dd71916
Ivo van Heel <heel@glr.nl>
2021-10-10 18:11:21 +0200

    Change save extension to .bg2save for BG2

--- BG1SaveSync/Classes/SaveGame.cs
+++ BG1SaveSync/Classes/SaveGame.cs
@@ -16,14 +16,14 @@ namespace BG1SaveSync.Classes
         public SaveGame(DirectoryInfo dirInfo)
         {
             Name = dirInfo.Name;
-            ZipName = $"{Name}.bg1save";
+            ZipName = $"{Name}.bg2save";
             Date = dirInfo.CreationTime;
         }
 
         public SaveGame(FileInfo fileInfo)
         {
             ZipName = fileInfo.Name;
-            Name = ZipName.Substring(0, ZipName.Length - ".bg1save".Length);
+            Name = ZipName.Substring(0, ZipName.Length - ".bg2save".Length);
             Date = fileInfo.CreationTime;
         }
 
@@ -51,7 +51,7 @@ namespace BG1SaveSync.Classes
             if (Directory.Exists(directory))
             {
                 List<SaveGame> saveGameList = new List<SaveGame>();
-                FileInfo[] saveFiles = new DirectoryInfo(directory).GetFiles("*.bg1save").OrderByDescending(p => p.CreationTime).ToArray();
+                FileInfo[] saveFiles = new DirectoryInfo(directory).GetFiles("*.bg2save").OrderByDescending(p => p.CreationTime).ToArray();
                 foreach (FileInfo file in saveFiles)
                 {
                     saveGameList.Add(new SaveGame(file));
